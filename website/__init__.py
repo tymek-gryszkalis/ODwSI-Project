ae7191294ac0b4f8df6cc1629a2f482483de42e8
Tymek Gryszkalis <tymek@fork.pl>
2023-12-15 17:46:57 +0100

    Added login manager + fixed secret key

--- website/__init__.py
+++ website/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 import os
 
 db = SQLAlchemy()
@@ -7,7 +8,7 @@ DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
-    app.config["SECRET KEY"] = "HEHE"
+    app.config["SECRET_KEY"] = "HEHE"
     app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
     db.init_app(app)
 
@@ -20,5 +21,13 @@ def create_app():
     from . import models
     with app.app_context():
         db.create_all()
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return models.User.query.get(int(id))
 
     return app
