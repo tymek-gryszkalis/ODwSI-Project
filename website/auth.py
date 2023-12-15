a13c0a0ba3fcdc5f5f4376c888655a929adaa96e
Tymek Gryszkalis <tymek@fork.pl>
2023-12-15 17:09:18 +0100

    Very basic signup (working)

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect, url_for
+from .models import User
+from . import db
 
 auth = Blueprint("auth", __name__)
 
@@ -7,8 +9,13 @@ def login():
     return render_template("login.html")
 
 @auth.route("/signup", methods=['GET', 'POST'])
-def logout():
+def signup():
     if request.method == "POST":
         email = request.form.get("email")
         pwd = request.form.get("pwd")
+        name = request.form.get("usr")
+        newUser = User(email = email, username = name, password = pwd)
+        db.session.add(newUser)
+        db.session.commit()
+        return redirect(url_for("auth.login"))
     return render_template("signup.html")
