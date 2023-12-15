22c23c304b4d08549a7c96d891e25a5213225373
Tymek Gryszkalis <tymek@fork.pl>
2023-12-15 17:47:11 +0100

    added basic login

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, request, redirect, url_for
+from flask_login import login_user, logout_user, current_user
 from .models import User
 from . import db
 
@@ -6,6 +7,14 @@ auth = Blueprint("auth", __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == "POST":
+        email = request.form.get("email")
+        pwd = request.form.get("pwd")
+        user = User.query.filter_by(email = email).first()
+        if user:
+            if pwd == user.password:
+                login_user(user)
+                return redirect(url_for("views.home"))
     return render_template("login.html")
 
 @auth.route("/signup", methods=['GET', 'POST'])
