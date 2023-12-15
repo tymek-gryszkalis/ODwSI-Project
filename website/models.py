f522f9fd095d5c685e69ea05a7e0c8203c93f1ca
Tymek Gryszkalis <tymek@fork.pl>
2023-12-15 17:59:10 +0100

    Added loan class

--- website/models.py
+++ website/models.py
@@ -6,3 +6,12 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(150), unique = True)
     username = db.Column(db.String(150))
     password = db.Column(db.String(150))
+
+class Loan(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Columne(db.String(150))
+    amount = db.Column(db.Float)
+    confirmed = db.Column(db.Boolean)
+    payed = db.Column(db.Boolean)
+    lender_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    borrower_id = db.Column(db.Integer, db.ForeignKey("user.id"))
