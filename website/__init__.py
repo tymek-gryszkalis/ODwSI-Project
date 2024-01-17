87a86383eadf1fec972fe9854ebaf73793bab672
Sudowiec <lalarsek@gmail.com>
2024-01-17 09:27:39 +0100

    email handling

--- website/__init__.py
+++ website/__init__.py
@@ -1,14 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-import os
+from flask_mail import Mail
+import json, os
 
 db = SQLAlchemy()
+mail = Mail()
 DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
-    app.config["SECRET_KEY"] = "HEHE"
+    j = open(os.getcwd() + '\\website\\static\\config.json')
+    app.config["SECRET_KEY"] = json.load(j)["key"]
     app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
     db.init_app(app)
 
@@ -29,5 +32,14 @@ def create_app():
     @login_manager.user_loader
     def load_user(id):
         return models.User.query.get(int(id))
+    
+    app.config['MAIL_SERVER']='smtp.gmail.com'
+    app.config['MAIL_PORT'] = 465
+    app.config['MAIL_USERNAME'] = 'lalarsek@gmail.com'
+    app.config['MAIL_PASSWORD'] = "deor kemw dpom tatl"
+    app.config['MAIL_USE_TLS'] = False
+    app.config['MAIL_USE_SSL'] = True
+    global mail
+    mail= Mail(app)
 
     return app
